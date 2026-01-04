2809df8cb85e9345f0182e21bb9c30828b0033d7
Al Fahad <af113664@gmail.com>
2026-01-04 19:02:19 +0600

    Standard ApiResponse and API errors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from "express";
+import cors from "cors"
 
-const app = express()
+const app = express();
+
+// Basic configuaratons
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended: true, limit:"16kb"}));
+app.use(express.static("public"));
+
+// cors configuaration 
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods : ["GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.get("/", (req, res) =>{
     res.send("App is running");
